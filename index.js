55262f0e857cadd82738240f4fe081354e1a01eb
Darbaz Ali <darbaz.a.h@gmail.com>
2020-05-22 17:23:26 +0300

    write pure functions

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
 console.log("Functional JavaScript");
+
+
+/* 
+    Pass Arguments to Avoid External Dependence in a Function
+
+    The functional Programming methodology is to create clean code that is clean from generating side effects
+
+*/
+
+
+let fixedValue = 4;
+
+// create a function that increametns it's input by 1
+const increamenter = ( value ) => {
+    return value + 1;
+}
+
+
+console.log(increamenter(fixedValue));
+console.log(increamenter(3));
+
+
+
+
+
+
+let baseArr = [
+    "Item 1",
+    "Item 2",
+    "Item 3",
+    "Item 4"
+];
+
+console.log(baseArr);
+
+
+const add = (arr, item ) => {
+    let newArr = [...arr]
+    newArr.push(item);
+    return newArr;
+}
+
+
+const newArr = add(baseArr, "Item 5")
+
+console.log(newArr);
+console.log(baseArr);
